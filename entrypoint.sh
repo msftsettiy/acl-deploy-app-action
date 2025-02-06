48053e8d586c32ff01052cb1dc3ef2cec6dd4639
msftsettiy <63882775+msftsettiy@users.noreply.github.com>
2025-02-06 11:43:02 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cp $1 /opt/ccf_sgx/bin/set_js_app.json
+cp $1 /opt/ccf_sgx/bin/bundle.json
 
 echo "$CERTD" > /opt/ccf_sgx/bin/cert
 echo "$KEYD" > /opt/ccf_sgx/bin/key
 
 cd /opt/ccf_sgx/bin
 echo "Submitting the JS app."
-response_code=$(curl -k -s -i -o response.txt -w  "%{response_code}" -L -X PUT ${CCF_URL}/app/userDefinedEndpoints?api-version=2024-08-22-preview -H "Content-Type: application/json" --data-binary set_js_app.json --cert cert --key key)
+response_code=$(curl -k -s -i -o response.txt -w  "%{response_code}" -L -X PUT ${CCF_URL}/app/userDefinedEndpoints?api-version=2024-08-22-preview -H "Content-Type: application/json" --data-binary "@/opt/ccf_sgx/bin/bundle.json" --cert cert --key key)
 echo "Response from ACL: $response_code"
 echo "response_code=$response_code" >> $GITHUB_OUTPUT
